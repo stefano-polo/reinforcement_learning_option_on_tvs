2c4ca9634f2380222d23c1c8fd9db4e5534dad7c
Stefano <stefano.teto96@gmail.com>
2020-04-16 10:46:28 +0200

    Implied volatility with error trial

--- Code/pricing.py
+++ Code/pricing.py
@@ -1,19 +1,31 @@
 import numpy as np
-import numpy as np
-import matplotlib.pyplot as plt
 import random as rnd
 from scipy.interpolate import interp1d
 import scipy.stats as si  #for gaussian cdf
 
 
+"""Functions for Black & Scholes Formula"""
 def d1(S, K, T, r, q, sigma):
+    """d_1 function for BS model"""
     return (np.log(S / K) + (r - q + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
 
-def Call_Closed(S, K, T, r, q, d_1, d_2):
+def d2(S, K, T, r, q, sigma):
+    """d_2 function for BS model"""
+    return  (np.log(S / K) + (r - q - 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+
+def Call_Closed(S, K, T, r, q, sigma):
+    """Closed form of Call Option for BS model"""
+    d_1 = d1(S, K, T, r, q, sigma)
+    d_2 = d2(S, K, T, r, q, sigma)
     return S * np.exp(-q * T) * si.norm.cdf(d_1, 0.0, 1.0) - K * np.exp(-r * T) * si.norm.cdf(d_2, 0.0, 1.0)
 
-def vega(S,T, q, d_1):
+def vega(S, K, T, r, q, sigma):
+    """Vega"""
+    d_1 = d1(S, K, T, r, q, sigma)
     return (1 / np.sqrt(2 * np.pi)) * S * np.exp(-q * T) * np.sqrt(T) * np.exp((-d_1 ** 2) * 0.5)
+
+
+"""Classes for my simulation"""
 
 class Curve:
 
@@ -26,21 +38,12 @@ class Curve:
         return self.curve(date)
 
 
-class DiscountingCurve(Curve):
-    def __init__(self, reference=None, discounts=None, dates=None):
-        self.reference = reference
-        self.discounts = interp1d(dates, discounts)
-
-    def curve(self, date):
-        return np.exp(-self.discounts(date)*date)
-
-
 class EquityForwardCurve(Curve):
 
     def __init__(self, spot=None, reference=None, discounting_curve=None,
                 repo_rates=None, repo_dates=None, dividend_rates=None, dividend_dates=None): #discounting_curve is a a DiscountingCurve type object
         self.spot = spot
-        self.reference = reference
+        self.reference = reference  #pricing date (t_0)
         self.discounting_curve = discounting_curve
         #self.repo_rates = interp1d(repo_dates, repo_rates)  #Linear interpolation inside the constructor since it is done only once
         #self.dividend_rates = interp1d(dividend_dates, dividend_rates)
@@ -51,12 +54,26 @@ class EquityForwardCurve(Curve):
 
 
 
+class DiscountingCurve(Curve):
+
+    def __init__(self, reference=None, discounts=None, dates=None):
+
+        self.reference = reference
+        self.discounts = interp1d(dates, discounts)
+
+    def curve(self, date):
+        return np.exp(-self.discounts(date-self.reference)*(date-self.reference))
+
+
 class PricingModel:
+
     def __init__(self, **kwargs):
-        raise Exception('model not implemented.')
+
+      raise Exception('model not implemented.')
 
     def simulate(self, fixings=None, Nsim=1, seed=14, **kwargs):
-        raise Exception('simulate not implemented.')
+
+      raise Exception('simulate not implemented.')
 
 
 class Black(PricingModel):
@@ -65,7 +82,7 @@ class Black(PricingModel):
     def __init__(self, volatility=None, forward_curve=None, fixings=None, **kwargs):
         self.volatility = volatility
         self.forward_curve = forward_curve(fixings)  #K/F_0(T)
-        self.fixings = fixings
+        self.fixings = fixings# - forward_curve.reference  #self.fixings = t_i - t_0
 
     def setSeed(self, seed):
         rnd.seed(seed)
@@ -73,28 +90,30 @@ class Black(PricingModel):
     def simulate(self, Nsim,**kwargs): #simulation of Martingale
         self.martingale = np.zeros((Nsim,len(self.fixings)))
         for i in range (Nsim):
-            self.martingale[i] = np.exp(-0.5*(self.volatility**2)*self.fixings[:]+self.volatility*np.sqrt(self.fixings[:])*rnd.gauss(0,1))
+            for j in range (len(self.fixings)):
+                Z = rnd.gauss(0,1)
+                self.martingale[i][j] = np.exp(-0.5*(self.volatility**2)*self.fixings[j]+self.volatility*np.sqrt(self.fixings[j])*Z)
 
-    def Call_PayOff(self,strike): #Monte Carlo call
+    def Call_PayOff(self,strike): #Monte Carlo call payoff
         self.h = strike/self.forward_curve
         self.pay = self.martingale- self.h
-        for i in range (len(self.fixings)):
-            for j in range (len(self.martingale)):
-                self.pay[j][i] = max(self.pay[j][i],0)
+        for i in range (len(self.martingale)):
+            for j in range (len(self.fixings)):
+                self.pay[i][j] = max(self.pay[i][j],0)
 
-
-    def newton_vol_call_div(self, S, C, i, r=0, q=0):
+    def newton_implied_volatility(self, S, C, i, r=0, q=0):
         """Implied volatility"""
         xold = 0
         xnew = np.sqrt((2*np.pi)/self.fixings[i])*(C[i]/S[i])
         tolerance = 0.0001
-
+        counter = 0
         while abs(xnew - xold) > tolerance:
             xold = xnew
-            d_1 = d1(S[i], self.h[i], self.fixings[i], r, q, xold)
-            d_2 = d_1 - xold*np.sqrt(self.fixings[i])
-            f = Call_Closed(S[i], self.h[i], self.fixings[i], r, q, d_1, d_2)
-            v = vega(S[i], self.fixings[i], q, d_1)
+            xold = xnew
+            f = Call_Closed(S[i], self.h[i], self.fixings[i], r, q, xold)
+            v = vega(S[i], self.h[i], self.fixings[i], r, q, xold)
             xnew = xold - (f-C[i])/ v
-
+            counter= counter +1
+            if(counter>=1000):
+                return None
         return abs(xnew)
--- Code/simulation.py
+++ Code/simulation.py
@@ -1,42 +1,64 @@
 import numpy as np
-import time, sys
-import pricing as p
-import MonteCarlo as MC
+from pricing import DiscountingCurve, EquityForwardCurve, Black
+from montecarlo import MC
 
 
 Nsim = int(1e6)
-volatility = 0.25
-spot = 10
-npoints = 150
-strike = np.linspace(0,200,npoints)
-maturities = np.linspace(0.0001,2,npoints)
+volatility = 20/100
+reference = 0
+spot = 150
+npoints = 100
+discount = 1/100
+strike = np.linspace(0,300,npoints)
+maturities = np.linspace(0.0001,3,npoints)
 
+"""Writing information file about simulation"""
+
+file1 = open("README.txt","w")
+file1.write("Black & Scholes model \n\n")
+file1.write("N Monte Carlo simulation: "+str(Nsim)+"\n")
+file1.write("Volatility: "+str(volatility)+" \n")
+file1.write("Costant discount factor: "+str(discount)+"\n")
+file1.write("Reference: "+str(reference)+"\n")
+file1.write("Spot price: "+str(spot)+"\n")
+file1.write("Number of maturities and strikes: "+str(npoints)+"\n")
+file1.write("Range of maturities ["+str(maturities[0])+", "+str(maturities[npoints-1])+"] \n")
+file1.write("Range of strikes ["+str(strike[0])+", "+str(strike[npoints-1])+"] \n")
+file1.close()
 
 r_dates = np.linspace(0,7,10)
-r = np.ones(10)*0.1
-discounting_curve = p.DiscountingCurve(None,r,r_dates)
-forward = p.EquityForwardCurve(spot,None,discounting_curve)
+r = np.ones(10)*discount
+discounting_curve = DiscountingCurve(reference,r,r_dates)
+forward = EquityForwardCurve(spot,reference,discounting_curve)
 
-bs = p.Black(volatility,forward,maturities)
+print("Martingale: Start \n")
+bs = Black(volatility,forward,maturities)
 bs.simulate(Nsim)
 X_T = np.mean(bs.martingale,axis=0)
-
+print("Martingale: Done \n")
+print("Saving martingale")
+#np.savetxt("Martingale_file_matrix.txt",bs.martingale)
+print("Martingale saved")
 
 implied_volatility = np.zeros((npoints,npoints))
+err_implied_volatility = np.zeros((npoints,npoints))
 start_time = time.time()
 for i in range(npoints):
     bs.Call_PayOff(strike[i])
+    mean, err = MC(bs.pay)
+    ext = mean+err
     print(i,"Calculating strike: ",strike[i])
-    mean, err = MC.Monte_Carlo_Result(bs.pay,100)
     for j in range (npoints):
-        implied_volatility[i][j] = bs.newton_vol_call_div(X_T, mean, j)
+        implied_volatility[i][j] = bs.newton_implied_volatility(X_T, mean, j)
+        err_implied_volatility[i][j] = bs.newton_implied_volatility(X_T, ext, j)
+        err_implied_volatility[i][j] = abs(implied_volatility[i][j]-err_implied_volatility[i][j])
 
 
-print("Tempo di esecuzione cella: ---%s minuti ---" %((time.time()-start_time)/60))
-
-
-
-np.savetxt("implied_volatility_"+str(npoints)+".txt",implied_volatility)
+print("Tempo di esecuzione algoritmo: ---%s minuti ---" %((time.time()-start_time)/60))
+print("Saving_Files...")
 K, T = np.meshgrid(strike,maturities)
-np.savetxt("K_matrix_for"+str(npoints),K)
-np.savetxt("T_matrix_for"+str(npoints),T)
+np.savetxt("implied_volatility_for_nKT"+str(npoints)+"_nMC"+str(Nsim)+".txt",implied_volatility)    #saving results
+np.savetxt("err_implied_volatility_for_nKT"+str(npoints)+"_nMC"+str(Nsim)+".txt",implied_volatility)
+np.savetxt("K_matrix_for_nKT"+str(npoints)+"_nMC"+str(Nsim)+".txt",K)
+np.savetxt("T_matrix_for_nKT"+str(npoints)+"_nMC"+str(Nsim)+".txt",T)
+print("COMPLETED")
