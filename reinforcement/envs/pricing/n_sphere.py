5ad6e1df785323c3b5228e593e9918df6ce5c3d3
Stefano <stefano.teto96@gmail.com>
2020-08-14 19:34:36 +0200

    Implementation of the renormalization of the agent action for the long and short constraint

--- reinforcement/envs/pricing/n_sphere.py
+++ reinforcement/envs/pricing/n_sphere.py
@@ -11,3 +11,19 @@ def n_sphere_to_cartesian(radius, angles):
     co = np.cos(a)
     co = np.roll(co, -1)
     return si*co*radius
+
+
+def sign_renormalization(vec, sum_plus, sum_minus):
+    """renormalize positive and negative elements of an array separately if necessary such 
+    that sum of positive is equal to sum_plus and the abs sum of negative is equal to sum_neg"""
+    pos = vec>=0
+    neg = (1-vec)>0
+    p_s = np.sum(vec[pos])
+    n_s = abs(np.sum(vec[neg]))
+    if p_s > sum_plus:
+        vec[pos] = (vec[pos]/p_s)*sum_plus
+    if n_s > sum_minus:
+        vec[neg] = (vec[neg]/n_s)*sum_minus
+    p_s = np.sum(vec[pos])
+    n_s = abs(np.sum(vec[neg]))
+    return vec
