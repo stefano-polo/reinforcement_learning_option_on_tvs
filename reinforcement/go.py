ec7251e7c063943b0ed11188be51e30cd7fbdc3a
Stefano Polo <stefano.polo@intesasanpaolo.com>
2021-05-13 16:37:32 +0200

    .

--- reinforcement/go.py
+++ reinforcement/go.py
@@ -132,14 +132,13 @@ if __name__ == '__main__':
         num_hidden='8',
         num_env='27',
         lr='3e-4',
-        gamma='0.9',  
+        gamma='0.85',  
         train_timesteps='8e7',
         test_episodes='1e6',
         print_episodes='1',
         print_period='64',
         save_interval='200',    
         value_network='copy',
-        noise='7.',
         beta='0.7',
         custom_suffix='_freestrategy_displacedmarket_2assets_monthgrid_maturity2_strikeatm'   #test on one_month 6,10,1e6
         )
