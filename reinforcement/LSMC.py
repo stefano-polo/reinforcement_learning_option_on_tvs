e1656d32c9def470d847a04dbd81abad745237be
Stefano Polo <stefano.polo@intesasanpaolo.com>
2021-06-04 17:59:51 +0200

    LSMC: error correction for the coeff_matrix idex

--- reinforcement/LSMC.py
+++ reinforcement/LSMC.py
@@ -29,9 +29,16 @@ def alpha_vector(a1, nu, Nsim=1, norm=1):
     beta = nu[1,0,:]**2 + nu[1,1,:]**2
     gamma = a_1*(nu[0,0,:]*nu[1,0,:]+ nu[0,1,:]*nu[1,1,:])
     delta = (a_1**2)*(nu[0,0,:]**2+nu[0,1,:]**2) - norm
+    #numerator = norm*(nu[1,0]**2+nu[1,1]**2)
+    #denominator = (nu[0,0,:]*nu[1,1,:])**2+(nu[1,0,:]*nu[0,1,:])**2-2.*nu[0,0,:]*nu[1,1,:]*nu[1,0,:]*nu[0,1,:]
+    #ratio = numerator/denominator
+    #print("a_1^2 ",a1**2)
+    #print("ratio ",ratio)
     Delta = gamma**2-beta*delta
-    Delta[Delta<0] = 0.
-    a_2 = (-gamma+np.sqrt(Delta))/beta
+    #print("delta ",Delta)
+    #Delta[Delta<0] = 0.
+
+    a_2 = (-gamma-np.sqrt(Delta))/beta
     
     return np.array(([a_1,a_2])).T
 
@@ -48,11 +55,8 @@ def evolve_TVS(index_start, I_t, dS_S, Vola, alpha, target_vola, instant_interes
     s = np.sum(alpha, axis=1)
     I_t_plus_1 = np.ones(dS_S[:,0,0].shape)*I_t
     for t in range(index_start, N_euler_grid+index_start):
-        if t==index_start:
-            omega = target_vola
-        else:
-            nu = nu_matrix(Vola[:,t,:],identity_3d, corr_chole, N_equity, Nsim)
-            omega = target_vola/np.linalg.norm(alpha_dot_nu(alpha, nu, N_equity, Nsim),axis=1)
+        nu = nu_matrix(Vola[:,t,:],identity_3d, corr_chole, N_equity, Nsim)
+        omega = target_vola/np.linalg.norm(alpha_dot_nu(alpha, nu, N_equity, Nsim),axis=1)
         I_t_plus_1 = I_t_plus_1 * (1. + np.sum(alpha*dS_S[:,t,:],axis=1)*omega + dt * instant_interest_rate[t]*(1.-omega*s))
     return I_t_plus_1
 
@@ -93,8 +97,11 @@ Nsim = 1e3  #number of MC paths
 N_mc_forward = 1e6
 print_logs=True
 save_coeff = True
-number_I = 100
-number_alpha=100
+load_coeff = False
+R = 1.
+name = ""
+number_I = 300
+number_alpha=300
 polynomial_basis = lambda x:  np.column_stack((x, x**2, x**3))
 
 
@@ -108,8 +115,9 @@ identity_3d[idx, idx, :] = 1
 
 ##Problem grids
 I_grid = np.linspace(0.001*I_0,I_0*10,number_I)
-alpha_grid = np.linspace(-5.,5.,number_alpha)
-    
+#alpha_grid = np.linspace(-1.,1.,number_alpha)
+alpha_grid = np.linspace(0.01, np.pi*2, number_alpha) 
+   
 ACT = 365.0
 if frequency == "month":
     month_dates = np.array([31.,28.,31.,30.,31.,30.,31.,31.,30.,31.,30.,31.])
@@ -141,7 +149,10 @@ euler_grid = model.time_grid
 r_t = D.r_t(np.append(0.,euler_grid[:-1]))
 dt_vector = model.dt
 np_random = np.random
+Polynomial_grade = len(polynomial_basis(1.).T)
 
+coeff_matrices = np.zeros((len(I_grid),len(t_grid[:-1]), len(alpha_grid), Polynomial_grade+1))
+    
 start_time = time.time()
 ##MC simulation of the risky assets 
 np_random.seed(Seed)
@@ -151,14 +162,11 @@ dS_S = (S[:,1:,:]-S[:,:-1,:])/S[:,:-1,:]
 
 omega = target_volatility * np.ones(Nsim)   #since we parameterize the action such that the norm(alpha@nu)=1
 
-Polynomial_grade = len(polynomial_basis(1.).T)
-coeff_matrices = np.zeros((len(I_grid),len(t_grid), len(alpha_grid), Polynomial_grade+1))
 V_t_plus = np.zeros((Nsim, len(I_grid)))
 V_T = np.zeros((V_t_plus.shape))
 state_index_rev = list(reversed(state_index[:-1]))
 V_t = np.ones(V_t_plus.shape)*(-np.inf)
 
-###Domande roberto: interpolazione a riga 137, un solo asset, che driver uso nella regressione?dS_S, S o W? che range di griglia uso per I e alpha
 k = 0
 i=0
 for I_T in I_grid:
@@ -170,28 +178,30 @@ for t in reversed(t_grid[:-1]):
     start_index = state_index_rev[k]
     index_I_grid = 0
     nu = nu_matrix(simulations_Vola[:,start_index,:],identity_3d, correlation_chole, N_equity, Nsim)
+    states = np.log(S[:,start_index,0]/spot_prices[0]) #first asset 
+    Y = polynomial_basis(states)
+    Y = sm.add_constant(Y)
     for It in I_grid:
         alpha_index = 0
         for a in alpha_grid:
-            alpha = alpha_vector(a,nu,Nsim)
+            A = a*np.ones(Nsim)
+            alpha = np.array(([R*np.cos(A),R*np.sin(A)])).T#alpha_vector(a, nu, Nsim, norm=5)# np.array(([A, np.sqrt(1.-A**2)])).T
+           # print(alpha)
             #print(alpha)
             I_t_plus_1 = evolve_TVS(start_index, It, dS_S, simulations_Vola, alpha, target_volatility, r_t, dt_vector[-(k+1)], N_euler_grid, N_equity, Nsim, identity_3d, correlation_chole)
             V_realized = np.diag(interp1d(I_grid, V_t_plus, axis=1, fill_value="extrapolate")(I_t_plus_1))
             ##Regression
-            states = np.log(S[:,start_index,0]/spot_prices[0]) #first asset 
-            Y = polynomial_basis(states)
-            Y = sm.add_constant(Y)
             ols = sm.OLS(V_realized, Y)
             ols_result = ols.fit()
             coeff_It_t = ols_result.params
-            coeff_matrices[index_I_grid, k, alpha_index,:] = coeff_It_t   #saving regression coeff
+            coeff_matrices[index_I_grid, len(t_grid[:-1])-k-1, alpha_index,:] = coeff_It_t   #saving regression coeff
             ##Continuation Value
             V_alpha = np.sum(coeff_It_t * Y, axis=1)      #continuation value
+           # print("Continuation ",V_alpha)
             V_t[:,index_I_grid] =  np.maximum(V_t[:,index_I_grid], V_alpha)
-        if(((It == I_grid[2]) or (It == I_grid[3]) or (It == I_grid[-1])) and (print_logs==True)):
-            plot_regression(states, V_realized, V_alpha, It, a, t)
-        
             alpha_index +=1
+        if(((It == I_grid[2]) or (It == I_grid[0]) or (It == I_grid[-1])) and (print_logs==True)):
+            plot_regression(states, V_realized, V_alpha, It, a, t)
         index_I_grid +=1
     if print_logs:
         print(" Completed Month ",k," of ",T*12)
@@ -200,15 +210,19 @@ for t in reversed(t_grid[:-1]):
     k += 1
 
 if save_coeff:
-    np.save("Price_paths",V_t)
-    np.save("Regression_coeff",coeff_matrices)
+    np.save(name+"Price_paths",V_t)
+    np.save(name+"Regression_coeff",coeff_matrices)
 
 
 if print_logs:
     t_LSMC = (time.time()-start_time)/60.
     print("--- Execution time for LSMC regression: ", t_LSMC, " min ---")
     print("Strating forward MC")
+
+###Forward Monte Carlo
 shape = (N_equity,N_equity,N_mc_forward)
+
+
 ### Array of identity matrices for vectorized calculations
 identity_3d = np.zeros(shape)
 idx = np.arange(shape[0])
@@ -218,18 +232,20 @@ S_t = np.ones((N_mc_forward,N_equity))*spot_prices
 alpha_best = np.array([])
 nu = np.array([])
 for i in range(len(t_grid[:-1])):
-    print("Month ",i)
+    #if print_logs:
+       # print("Month ",i)
     V_max = np.ones(N_mc_forward)*(-np.inf)
     alpha_chosen = np.zeros(N_mc_forward)
-    coeff_matrices_time =  coeff_matrices[:,i,:,:]
+    coeff_matrices_time =  coeff_matrices[:,i,:,:]      #shape(np.zeros((len(I_grid),len(t_grid), len(alpha_grid), Polynomial_grade+1)))
+    states = np.log(S_t[:,0]/spot_prices[0]) #first asset 
+    #print(states)
+    Y = polynomial_basis(states)
+    Y = sm.add_constant(Y)
     a_index = 0
     for a in alpha_grid:
         coeff_matrices_time_alpha = coeff_matrices_time[:,a_index,:]
         coefficients = interp1d(I_grid, coeff_matrices_time_alpha, axis=0, fill_value="extrapolate")(I_t) 
         a_index += 1
-        states = np.log(S_t[:,0]/spot_prices[0]) #first asset 
-        Y = polynomial_basis(states)
-        Y = sm.add_constant(Y)
         expected_continuation = np.sum(coefficients * Y, axis=1)
         alpha_chosen[expected_continuation > V_max] = a
         V_max = expected_continuation
@@ -241,7 +257,7 @@ for i in range(len(t_grid[:-1])):
         S_t_plus_1, sigma_t = evolve_martingale(S_t, LV, np_random, t_in, t_fin, correlation_chole, F, N_mc_forward, N_equity)
         nu = nu_matrix(sigma_t,identity_3d, correlation_chole, N_equity, N_mc_forward)
         if j==0:
-            alpha_best =  alpha_vector(alpha_chosen,nu,N_mc_forward)
+            alpha_best = np.array(([R*np.cos(alpha_chosen),R*np.sin(alpha_chosen)])).T#alpha_vector(a, nu, Nsim, norm=5)# np.array(([A, np.sqrt(1.-A**2)])).T
             s = np.sum(alpha_best,axis=1)
         dS_S = (S_t_plus_1-S_t)/S_t
         omega = target_volatility/np.linalg.norm(alpha_dot_nu(alpha_best, nu, N_equity, N_mc_forward),axis=1)
